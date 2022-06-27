04a21eedb4220eefd35fe055861e358362b23745
noltron000 <noltron000@gmail.com>
2022-06-27 13:01:18 -0700

    Start an express server
    This is an MVP of connecting GraphQL! Note the lack of Apollo: it's not needed for something basic like this.

---
+++ src/root-values.ts
@@ -0,0 +1,8 @@
+// Create a root resolver. These are defaults for the db.
+const projects = [
+	{name: 'Suprez'},
+	{name: 'Maze Generator'},
+	{name: 'Herd Immunity'},
+]
+
+export const rootValue = {projects}
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,33 @@
-console.info('Hello World!')
+import fs from 'fs'
+import express from 'express'
+import {graphqlHTTP} from 'express-graphql'
+import {buildSchema} from 'graphql'
+import {rootValue} from './root-values.js'
 
-export default undefined
+const SCHEMA_FILE = 'src/schema.gql'
+const PORT = 4000
+
+const main = async ( ) => {
+	const app = express( )
+
+	// Import the schema's data types and build it with GraphQL.
+	const rawSchema = await fs.promises.readFile(SCHEMA_FILE, 'utf8')
+	const schema = buildSchema(rawSchema)
+
+	// Feed options to the express server.
+	const options = {
+		graphiql: true,
+		schema: schema,
+		rootValue: rootValue,
+	}
+
+	app.use(graphqlHTTP(options))
+
+	// Finally, start the express server.
+	app.listen(PORT, ( ) => {
+		console.info(`Server started on port ${PORT}.`)
+		console.info(`http://localhost:${PORT}/`)
+	})
+}
+
+void main( )
