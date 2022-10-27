0323d339a94be69e9f80938609fc252a17dbfd7f
noltron000 <noltron000@gmail.com>
2022-10-26 22:15:49 -0700

    Update .env example & types

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,12 +1,26 @@
 interface DotEnv {
+	// Override generic type-any variables to type-unknown
 	[key: string]: unknown,
+
+	// Basic Node variables
 	NODE_ENV: 'development' | 'production',
 	PORT: number,
+
+	// Database variables
 	DB_NAME: string,
 	DB_CLUSTER: string,
 	DB_USERNAME: string,
 	DB_PASSWORD: string,
 	DB_URL: string,
+
+	// GitHub App credentials
+	GH_INSTALLATION_ID: number,
+	GH_CLIENT_ID: string,
+	GH_APP_ID: number,
+	GH_CLIENT_SECRET: string,
+	GH_APP_PRIVATE_KEY: string,
+
+	// Nebuloid App authentication
 	JWT_SECRET: string,
 }
 
