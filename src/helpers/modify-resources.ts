91743c224220ac38f905b9c435519d88a058f308
noltron000 <noltron000@gmail.com>
2025-03-22 19:03:16 -0400

    Improve token generation logic
    Allows sessions to be passed through.

--- src/helpers/modify-resources.ts
+++ src/helpers/modify-resources.ts
@@ -1,15 +1,33 @@
 import {RefreshTokens} from '@app/models'
 import {UserDbObject} from '@app/types/generated/schema'
+import {ClientSession} from 'mongoose'
 import {signAccessToken, signRefreshToken} from './sign-jwt'
 
 /**
 This function first creates a new refresh token in the
 	database, but then also signs a pair of tokens to return.
 **/
-const generateTokens = async (user: UserDbObject) => {
-	// Create a new refresh token in the database.
+const generateTokens = async (
+	user: UserDbObject,
+	session?: ClientSession,
+) => {
 	const userId = user._id.toString( )
-	const refreshTokenDoc = await RefreshTokens.create({owner: userId})
+
+	// Create a new refresh token in the database.
+	// Conditionally use the given params
+	//  to give back the correct function call.
+	let refreshTokenDoc
+	if (session == null) {
+		refreshTokenDoc = await RefreshTokens.create(
+			{owner: userId},
+		)
+	}
+	else {
+		[refreshTokenDoc] = await RefreshTokens.create(
+			[{owner: userId}],
+			{session},
+		)
+	}
 
 	// Sign the access and refresh tokens.
 	const refreshTokenId = refreshTokenDoc._id.toString( )
